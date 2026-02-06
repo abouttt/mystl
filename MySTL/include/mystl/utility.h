e6633fc2d91a39f232d4312c52ca0f50251017cf
Daehan Jang <abouttt57@gmail.com>
2026-02-06 14:27:22 +0900

    Remove cstddef.h, type_traits.h

--- MySTL/include/mystl/utility.h
+++ MySTL/include/mystl/utility.h
@@ -1,69 +1,70 @@
 #pragma once
 
-#include "cstddef.h"
-#include "type_traits.h"
+#include <type_traits>
 
 namespace mystl
 {
 
 template <typename T>
-constexpr remove_reference_t<T>&& move(T&& t) noexcept
+constexpr std::remove_reference_t<T>&& move(T&& t) noexcept
 {
-	return static_cast<remove_reference_t<T>&&>(t);
+	return static_cast<std::remove_reference_t<T>&&>(t);
 }
 
 template <typename T>
-constexpr conditional_t<is_nothrow_move_constructible_v<T> || !is_copy_constructible_v<T>, const T&, T&&>
+constexpr std::conditional_t<std::is_nothrow_move_constructible_v<T> || !std::is_copy_constructible_v<T>, const T&, T&&>
 move_if_noexcept(T& x) noexcept
 {
 	return move(x);
 }
 
-template <typename T> requires is_move_constructible_v<T> && is_move_assignable_v<T>
-constexpr void swap(T& a, T& b) noexcept(is_nothrow_constructible_v<T> && is_nothrow_assignable_v<T>)
+template <typename T>
+	requires std::is_move_constructible_v<T> && std::is_move_assignable_v<T>
+constexpr void swap(T& a, T& b) noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_move_assignable_v<T>)
 {
 	T tmp = move(a);
 	a = move(b);
 	b = move(tmp);
 }
 
-template <typename T2, size_t N> requires is_swappable_v<T2>
-constexpr void swap(T2(&a)[N], T2(&b)[N]) noexcept(is_nothrow_swappable_v<T2>)
+template <typename T2, std::size_t N>
+	requires std::is_swappable_v<T2>
+constexpr void swap(T2(&a)[N], T2(&b)[N]) noexcept(std::is_nothrow_swappable_v<T2>)
 {
 	if (&a == &b)
 	{
 		return;
 	}
 
-	for (size_t i = 0; i < N; ++i)
+	for (std::size_t i = 0; i < N; ++i)
 	{
 		swap(a[i], b[i]);
 	}
 }
 
 template <typename T>
-constexpr T&& forward(remove_reference_t<T>& t) noexcept
+constexpr T&& forward(std::remove_reference_t<T>& t) noexcept
 {
 	return static_cast<T&&>(t);
 }
 
 template <typename T>
-constexpr T&& forward(remove_reference_t<T>&& t) noexcept
+constexpr T&& forward(std::remove_reference_t<T>&& t) noexcept
 {
-	static_assert(!is_lvalue_reference_v<T>, "cannot forward rvalue as lvalue");
+	static_assert(!std::is_lvalue_reference_v<T>, "cannot forward rvalue as lvalue");
 	return static_cast<T&&>(t);
 }
 
 template <typename T, typename U = T>
-constexpr T exchange(T& obj, U&& new_value) noexcept(is_nothrow_move_constructible_v<T> && is_nothrow_assignable_v<T&, U>)
+constexpr T exchange(T& obj, U&& new_value) noexcept(std::is_nothrow_move_constructible_v<T> && std::is_nothrow_assignable_v<T&, U>)
 {
-	T old_value = move(obj);
-	obj = forward<U>(new_value);
+	T old_value = static_cast<T&&>(obj);
+	obj = static_cast<U&&>(new_value);
 	return old_value;
 }
 
 template <typename T>
-constexpr add_const_t<T>& as_const(T& t) noexcept
+constexpr std::add_const_t<T>& as_const(T& t) noexcept
 {
 	return t;
 }
@@ -72,7 +73,7 @@ template <typename T>
 void as_const(const T&&) = delete;
 
 template <typename T>
-add_rvalue_reference_t<T> declval() noexcept
+std::add_rvalue_reference_t<T> declval() noexcept
 {
 	static_assert(false, "declval not allowed in an evaluated context");
 }
--- Tests/main.cpp
+++ Tests/main.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 
-#include "mystl/cstddef.h"
-#include "mystl/type_traits.h"
 #include "mystl/utility.h"
 
 int main()
 {
+    
 }
